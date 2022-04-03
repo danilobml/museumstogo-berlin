2d80d51a548360ee24436b02d1573bcdaed4c5df
danilobml <danbarml@gmail.com>
2022-04-03 09:46:13 +0200

    datalist with all Berlin museums to choose from

---
+++ src/Components/InputForm/MuseumsInput.js
@@ -0,0 +1,15 @@
+const MuseumsInput = (props) => {
+    return (
+        <>
+        <label for="list-museum">Or choose from a list of museums in Berlin:</label>
+        <br />
+        <input list="museums" name="list-museum" id="list-museums"></input>
+        <datalist id="museums" name="museums">
+            {props.museumList.map(museum => 
+            <option key= {museum} value={museum}>{museum}</option>)}
+        </datalist>
+        </>
+    )
+}
+
+export default MuseumsInput;
