d95a6e80d7f40bdc4a595b3e8362f85387315af4
danilobml <danbarml@gmail.com>
2022-04-09 18:03:29 +0200

    end first day

--- src/Components/List/Delete.js
+++ src/Components/List/Delete.js
@@ -1,7 +1,11 @@
-import deleteIcon from "./delete.svg"; 
+import deleteIcon from "./delete.svg";
 
-const Delete = () => {
-    return <span><img src={deleteIcon} alt="delete icon" className="icon" /></span>
-}
+const Delete = ({ deleteMuseum, name }) => {
+  return (
+    <span>
+      <img src={deleteIcon} alt="delete icon" className="icon" onClick={() => deleteMuseum(name)} />
+    </span>
+  );
+};
 
 export default Delete;
--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,18 +1,26 @@
-import "./List.css"
-import Delete from './Delete'
-import Edit from './Edit'
+import "./List.css";
+import Delete from "./Delete";
+import Edit from "./Edit";
 
-const List = ( {museums} ) => {
-    return (
+const List = ({ handleComplete, museums, deleteMuseum }) => {
+  return (
     <div className="container">
-        <ul>
-            {museums.map(museum => (
-                <li key={museum.index} className="list-item"><input type="checkbox"></input>{museum.name}<Edit /><Delete /> </li>
-            ))}
-        </ul>
+      <ul>
+        {museums.map((museum, index) =>
+          museum ? (
+            <li key={index} style={{ textDecoration: museum.completed ? "line-through" : " " }} className="list-item">
+              <input type="checkbox" onChange={() => handleComplete(museum.name)} checked={museum.completed}></input>
+              {museum.name}
+              <Edit />
+              <Delete deleteMuseum={deleteMuseum} name={museum.name} />{" "}
+            </li>
+          ) : (
+            ""
+          )
+        )}
+      </ul>
     </div>
-    )
+  );
 };
 
 export default List;
-
